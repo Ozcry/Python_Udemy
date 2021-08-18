da5661ae480c4a1c13cff67b266027989626ca45
Ozcry <jean.lucasferreria1998@gmail.com>
2021-08-18 18:42:01 -0300

    att

---
+++ section_2/023 - Input: Entrada de dados do usuário/023.py
@@ -0,0 +1,10 @@
+nome = str(input("Qual o seu nome? "))
+idade = int(input("Qual a sua idade? "))
+ano_nascimento = 2021 - idade
+print(f'O usuário digitou {nome} e o tipo da variavel é {type(nome)}')
+print(f'{nome} tem {idade} anos')
+print(f'ele nasceu em {ano_nascimento}')
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+print(n1 + n2)
